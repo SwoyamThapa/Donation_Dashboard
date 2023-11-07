b3c619f3fb9f72cd038fb9935ef2bde846db073c
Swoyam Thapa <thapaswoyam123@gmail.com>
2023-11-06 22:29:35 -0600

    changes

--- view_test.py
+++ view_test.py
@@ -1,11 +1,10 @@
 import json
 import pandas as pd
 import dash
-from dash import dcc,html
+from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
-
 
 # Specify the file path to your locally saved JSON file
 file_path = 'modules/data/data.json'
@@ -43,7 +42,7 @@ for idx, organization in enumerate(unique_organizations):
     color_mapping[organization] = color
     subset_df = df[df['Organization'] == organization]
     text = subset_df['Organization'] + '<br>' + subset_df['Country']
-    
+
     fig1.add_trace(go.Scattergeo(
         locationmode="country names",
         locations=subset_df['Country'],
@@ -53,24 +52,15 @@ for idx, organization in enumerate(unique_organizations):
     ))
 
 fig1.update_layout(
-    title = "Organization's operatiopn country",
+    title="Organization's operation country",
     geo=dict(showland=True, landcolor="lightgray"),
     legend_title_text='Organizations',  # Customize the legend title
 )
-
-#fig1.show()
-
-
-df = pd.DataFrame(data)
 
 # Create a bar chart with 'goal' and 'funding' bars side by side
 fig2 = px.bar(df, x='title', y=['goal', 'funding'], title='Project Funding vs. Goal',
              labels={'title': 'Organization', 'value': 'Amount'},
              barmode='group')  # Set barmode to 'group'
-
-# Show the chart
-#fig.show()
-
 
 projects = []
 
@@ -78,9 +68,9 @@ for entry in data:
     org = entry.get('organization')
 
     projects_info = {
-        'organization' : org.get('name'),
-        'totalProjects' : org.get('totalProjects'),
-        'activeProjects' : org.get('activeProjects')
+        'organization': org.get('name'),
+        'totalProjects': org.get('totalProjects'),
+        'activeProjects': org.get('activeProjects')
     }
 
     if not projects_info in projects:
@@ -90,27 +80,16 @@ df = pd.DataFrame(projects)
 
 # Create a bar chart with 'goal' and 'funding' bars side by side
 fig3 = px.bar(df, x='organization', y=['totalProjects', 'activeProjects'], title='TotalProjects and ActiveProjects',
-             labels={'title': 'Organization', 'value': 'No. of Projects'},
-             barmode='group')  # Set barmode to 'group'
-
-# Show the chart
-#fig.show()
+              labels={'title': 'Organization', 'value': 'No. of Projects'},
+              barmode='group')  # Set barmode to 'group'
 
 app = dash.Dash(__name__)
 
-# app.layout = html.Div([
-#     html.Div(dcc.Graph(figure=fig1), style={'width': '33%', 'display': 'inline-block'}),
-#     html.Div(dcc.Graph(figure=fig2), style={'width': '33%', 'display': 'inline-block'}),
-#     html.Div(dcc.Graph(figure=fig3), style={'width': '33%', 'display': 'inline-block'}),
-# ])
-
 app.layout = html.Div([
-    html.Div(dcc.Graph(figure=fig1)),
-    html.Div(dcc.Graph(figure=fig2)),
-    html.Div(dcc.Graph(figure=fig3)),
+    html.Div(dcc.Graph(figure=fig1), style={'width': '33%', 'display': 'inline-block'}),
+    html.Div(dcc.Graph(figure=fig2), style={'width': '33%', 'display': 'inline-block'}),
+    html.Div(dcc.Graph(figure=fig3), style={'width': '33%', 'display': 'inline-block'}),
 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
